26c7c0f19ce814751522d91f8e71bf40d3eb331b
Florian <florian@flova.de>
2022-08-16 13:41:48 +0200

    Adjust training for mlp

--- behavior_clone/train.py
+++ behavior_clone/train.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader, random_split
 from profilehooks import profile
 
-from behavior_clone.model import EyePredModel1
+from behavior_clone.model import EyePredModel1, MLPSeq
 from behavior_clone.utils.datasets.dataset import RrcDataset
 from behavior_clone.utils.transforms import DEFAULT_TRANSFORMS
 from behavior_clone.utils import viz
@@ -26,10 +26,10 @@ def train(model):
     debug_show = False
 
     # Get Dataset
-    train_set = RrcDataset("C:\\Users\\florian\\rrc\\gen_data\\", 10)
+    train_set = RrcDataset("/home/florian/Projekt/bitbots/running_robot_2022/gen_data", 5)
 
     # Create Dataloader
-    train_data_loader = DataLoader(train_set, batch_size=64, num_workers=24, prefetch_factor=2, shuffle=True)
+    train_data_loader = DataLoader(train_set, batch_size=64, num_workers=14, prefetch_factor=2, shuffle=True)
 
     # Create Optimizer, Scheduler, ...
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=lr)
@@ -46,26 +46,12 @@ def train(model):
             # Run model
             output = model(data)
             # Calculate loss
-            loss = criterion(output.reshape(-1, 5), targets.long().reshape(-1))
+            loss = criterion(output, targets[:, -1].long())
             print(loss.detach().cpu())
-            print(torch.argmax(output[0], axis=1).detach().cpu().tolist())
-            print(targets[0].detach().cpu().long().tolist())
+            print(torch.argmax(output, axis=1).detach().cpu().tolist())
+            print(targets[:, -1].detach().cpu().long().tolist())
             # Calc gradients
             loss.backward()
-
-            # Make a viz pintout to a window or folder showing the image, target and prediction for a sequence
-            if i % debug_show_interval == 0 and False:
-                # Iterate over sequence dimension
-                for si in range(0, int(data.size(1))):
-                    # Draw both on the image
-                    canvas = viz.draw_pred_and_target(data[0,si], output[0,si], targets[0,si])
-                    # Show or save image
-                    if debug_show:
-                        cv2.imshow(f"debug", canvas)
-                        cv2.waitKey(1)
-                        time.sleep(0.1)
-                    else:
-                        cv2.imwrite(f"viz/debug_{i:08d}_{si:03d}.jpg", canvas)
 
             # After #gradient_accumulation steps optimize weights
             if i % gradient_accumulations == 0:
@@ -83,9 +69,9 @@ def train(model):
 if __name__ == "__main__":
     print("Load model")
     img_size = (129, 160)
-    max_len = 10
+    max_len = 5
     token_size=128
-    model = EyePredModel1(img_size=img_size, token_size=token_size, max_len=max_len)
+    model = MLPSeq(img_size=img_size, token_size=token_size, max_len=max_len)
     model.to(DEVICE)
     # Show summary
     train(model)
